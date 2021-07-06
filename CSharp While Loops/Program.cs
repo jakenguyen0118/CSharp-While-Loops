eda135c4a77b141e56e528d85b7613a90aa27389
Jake <jakenguyen0118@gmail.com>
2021-07-06 13:26:27 -0400

    Add project files.

---
+++ CSharp While Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharp_While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+
+            //    i++;
+            //}
+
+            while (true)
+            {
+                Console.Write("type your name:");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
